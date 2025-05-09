b82dfa415ffd31ac2cb54ebf20fa5e41e178ecaa
Phattharakorn Mahasorasak <foolproof.ja@gmail.com>
2025-05-09 13:23:12 +0700

    Remove unnecessary files

--- src/main.cpp
+++ src/main.cpp
@@ -73,106 +73,37 @@ void loadConfiguration(fs::FS& fs, const char* filename) {
     file.close();
 }
 
-// callback notifying us of the need to save config
 void saveConfigCallback() {
-    // Serial.println("Should save config");
     _delnF("Should save config");
     shouldSaveConfig = true;
 }
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial.println();
+    _serialBegin(115200);
 
-    // clean FS, for testing
-    // SPIFFS.format();
+    while (!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)) {
+        _delnF("Failed to initialize LittleFS library");
+        delay(1000);
+    }
 
-    // read configuration from FS json
-    Serial.println("mounting FS...");
-
-    //     if (SPIFFS.begin()) {
-    //         Serial.println("mounted file system");
-    //         if (SPIFFS.exists("/config.json")) {
-    //             //file exists, reading and loading
-    //             Serial.println("reading config file");
-    //             File configFile = SPIFFS.open("/config.json", "r");
-    //             if (configFile) {
-    //                 Serial.println("opened config file");
-    //                 size_t size = configFile.size();
-    //                 // Allocate a buffer to store contents of the file.
-    //                 std::unique_ptr<char[]> buf(new char[size]);
-
-    //                 configFile.readBytes(buf.get(), size);
-    // #if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-    //                 DynamicJsonDocument json(1024);
-    //                 auto                deserializeError = deserializeJson(json, buf.get());
-    //                 serializeJson(json, Serial);
-    //                 if (!deserializeError) {
-    // #else
-    //                 DynamicJsonBuffer jsonBuffer;
-    //                 JsonObject&       json = jsonBuffer.parseObject(buf.get());
-    //                 json.printTo(Serial);
-    //                 if (json.success()) {
-    // #endif
-    //                     Serial.println("\nparsed json");
-
-    //                     strcpy(mqtt_server, json["mqtt_server"]);
-    //                     strcpy(mqtt_port, json["mqtt_port"]);
-    //                     strcpy(api_token, json["api_token"]);
-
-    //                     if (json["ip"]) {
-    //                         Serial.println("setting custom ip from config");
-    //                         strcpy(static_ip, json["ip"]);
-    //                         strcpy(static_gw, json["gateway"]);
-    //                         strcpy(static_sn, json["subnet"]);
-    //                         Serial.println(static_ip);
-    //                     } else {
-    //                         Serial.println("no custom ip in config");
-    //                     }
-    //                 } else {
-    //                     Serial.println("failed to load json config");
-    //                 }
-    //             }
-    //         }
-    //     } else {
-    //         Serial.println("failed to mount FS");
-    //     }
+     _delnF("Loading configuration");
     loadConfiguration(LittleFS, filename);
 
-    // end read
-    // Serial.println(static_ip);
-    // Serial.println(api_token);
-    // Serial.println(mqtt_server);
     _deVar("ip: ", static_ip);
     _deVar(" | gw: ", static_gw);
     _deVar(" | sn: ", static_sn);
     _deVarln(" | dns: ", static_dns);
 
-    // The extra parameters to be configured (can be either global or just in the setup)
-    // After connecting, parameter.getValue() will get you the configured value
-    // id/name placeholder/prompt default length
-    // WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-    // WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
-    // WiFiManagerParameter custom_api_token("apikey", "API token", api_token, 34);
     WiFiManagerParameter customMqttBroker("broker", "mqtt server", mqttBroker, 16);
     WiFiManagerParameter customMqttPort("port", "mqtt port", mqttPort, 6);
     WiFiManagerParameter customMqttUser("user", "mqtt user", mqttUser, 10);
     WiFiManagerParameter customMqttPass("pass", "mqtt pass", mqttPass, 10);
 
-    // WiFiManager
-    // Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
 
-    // set config save notify callback
     wifiManager.setSaveConfigCallback(saveConfigCallback);
 
     // set static ip
-    // IPAddress _ip, _gw, _sn;
-    // _ip.fromString(static_ip);
-    // _gw.fromString(static_gw);
-    // _sn.fromString(static_sn);
-    // wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
     IPAddress _ip, _gw, _sn, _dns;
     _ip.fromString(static_ip);
     _gw.fromString(static_gw);
@@ -180,9 +111,6 @@ void setup() {
     _dns.fromString(static_dns);
     wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn, _dns);
     // add all your parameters here
-    // wifiManager.addParameter(&custom_mqtt_server);
-    // wifiManager.addParameter(&custom_mqtt_port);
-    // wifiManager.addParameter(&custom_api_token);
     wifiManager.addParameter(&customMqttBroker);
     wifiManager.addParameter(&customMqttPort);
     wifiManager.addParameter(&customMqttUser);
@@ -190,39 +118,25 @@ void setup() {
 
     // reset settings - for testing
     // wifiManager.resetSettings();
-
-    // set minimu quality of signal so it ignores AP's under that quality
-    // defaults to 8%
     wifiManager.setDarkMode(true);
-    // wifiManager.setMinimumSignalQuality(20);
-
-    // sets timeout until configuration portal gets turned off
-    // useful to make it all retry or go to sleep
-    // in seconds
+    // wifiManager.setMinimumSignalQuality(20); // Default 8%
+    // wifiManager.setConfigPortalTimeout(60);
+    // wifiManager.setConfigPortalBlocking(false);
     // wifiManager.setTimeout(120);
 
-    // fetches ssid and pass and tries to connect
-    // if it does not connect it starts an access point with the specified name
-    // here  "AutoConnectAP"
-    // and goes into a blocking loop awaiting configuration
-    // if (!wifiManager.autoConnect("AutoConnectAP", "password")) {
-    if (!wifiManager.autoConnect(deviceName, "password")) {
-        // Serial.println("failed to connect and hit timeout");
-        _delnF("failed to connect and hit timeout");
-        delay(3000);
-        // reset and try again, or maybe put it to deep sleep
-        ESP.restart();
-        delay(5000);
+    // if (!wifiManager.autoConnect(deviceName, "password")) {
+    //     _delnF("failed to connect and hit timeout");
+    //     delay(3000);
+    //     ESP.restart();
+    // }
+    // _delnF("WiFI is connected :D");
+     if (wifiManager.autoConnect(deviceName, "password")) {
+        _delnF("WiFI is connected :D");
+    } else {
+        _delnF("Configportal running");
     }
 
-    // if you get here you have connected to the WiFi
-    // Serial.println("connected...yeey :)");
-    _delnF("connected...yeey :)");
-
     // read updated parameters
-    // strcpy(mqtt_server, custom_mqtt_server.getValue());
-    // strcpy(mqtt_port, custom_mqtt_port.getValue());
-    // strcpy(api_token, custom_api_token.getValue());
     strcpy(mqttBroker, customMqttBroker.getValue());
     strcpy(mqttPort, customMqttPort.getValue());
     strcpy(mqttUser, customMqttUser.getValue());
@@ -230,34 +144,6 @@ void setup() {
 
     // save the custom parameters to FS
     if (shouldSaveConfig) {
-        //         Serial.println("saving config");
-        // #if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-        //         DynamicJsonDocument json(1024);
-        // #else
-        //         DynamicJsonBuffer jsonBuffer;
-        //         JsonObject&       json = jsonBuffer.createObject();
-        // #endif
-        //         json["mqtt_server"] = mqtt_server;
-        //         json["mqtt_port"]   = mqtt_port;
-        //         json["api_token"]   = api_token;
-
-        //         json["ip"]      = WiFi.localIP().toString();
-        //         json["gateway"] = WiFi.gatewayIP().toString();
-        //         json["subnet"]  = WiFi.subnetMask().toString();
-
-        //         File configFile = SPIFFS.open("/config.json", "w");
-        //         if (!configFile) {
-        //             Serial.println("failed to open config file for writing");
-        //         }
-
-        // #if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-        //         serializeJson(json, Serial);
-        //         serializeJson(json, configFile);
-        // #else
-        //         json.printTo(Serial);
-        //         json.printTo(configFile);
-        // #endif
-        //         configFile.close();
         File file = LittleFS.open(filename, "w");
         if (!file) {
             _delnF("Failed to open config file for writing");
@@ -293,10 +179,6 @@ void setup() {
         // end save
     }
 
-    // Serial.println("local ip");
-    // Serial.println(WiFi.localIP());
-    // Serial.println(WiFi.gatewayIP());
-    // Serial.println(WiFi.subnetMask());
     _deVar("ip: ", WiFi.localIP());
     _deVar(" | gw: ", WiFi.gatewayIP());
     _deVar(" | sn: ", WiFi.subnetMask());
@@ -307,5 +189,4 @@ void setup() {
 
 void loop() {
     statusLed.loop();
-    // put your main code here, to run repeatedly:
 }
